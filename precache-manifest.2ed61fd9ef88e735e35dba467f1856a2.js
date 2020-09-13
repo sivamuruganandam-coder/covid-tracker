e9150e18cf9d08c567941175e744266a3fc511ca
sivamuruganandam-coder <sivamuruganandam@gmail.com>
2020-09-13 11:28:57 +0530

    Updates

---
+++ precache-manifest.2ed61fd9ef88e735e35dba467f1856a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09865e11d5a82517faed9cbd5ceaa287",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "03685355281041c2fbd4",
+    "url": "/covid-tracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0a92d297295e2813b632",
+    "url": "/covid-tracker/static/css/main.9993747c.chunk.css"
+  },
+  {
+    "revision": "03685355281041c2fbd4",
+    "url": "/covid-tracker/static/js/2.96d3ff72.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/covid-tracker/static/js/2.96d3ff72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a92d297295e2813b632",
+    "url": "/covid-tracker/static/js/main.f4e23604.chunk.js"
+  },
+  {
+    "revision": "2136b6c2ad645fa39947",
+    "url": "/covid-tracker/static/js/runtime-main.951ff6e7.js"
+  }
+]);
